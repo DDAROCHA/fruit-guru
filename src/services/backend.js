9c528374abc7abe1ce9731061cdcbae85382b649
Diego Da Rocha <ddarocha00@gmail.com>
2025-08-27 08:55:51 -0300

    Ver12.0

--- src/services/backend.js
+++ src/services/backend.js
@@ -1,12 +1,19 @@
 import Backendless from "backendless";
 
-const APP_ID = "F405D13E-0A77-400C-ACBE-8146E8285936";
-const API_KEY = "BC70880E-34E2-4992-AB6C-C87592ED3A5B";
+//const APP_ID = "F405D13E-0A77-400C-ACBE-8146E8285936";
+//const API_KEY = "BC70880E-34E2-4992-AB6C-C87592ED3A5B";
+
+const APP_ID = "D949890C-0F54-4BDA-B167-8C493C07DD8D";
+const API_KEY = "9823F4DD-0CAD-40D0-8031-8602377D585D";
 
 Backendless.initApp(APP_ID, API_KEY);
 
+//const FLOWRUNNER_URL =
+//  "https://hotshotpower-us.backendless.app/api/automation/flow/E5756404-4189-49F3-BDE8-7FEE19F980DA/trigger/1666B887-3CC9-24F8-630C-26B80854583C/activate";
+
 const FLOWRUNNER_URL =
-  "https://hotshotpower-us.backendless.app/api/automation/flow/E5756404-4189-49F3-BDE8-7FEE19F980DA/trigger/1666B887-3CC9-24F8-630C-26B80854583C/activate";
+  "https://beefystone-us.backendless.app/api/automation/flow/1C309823-D37C-460D-BBE0-A2E5AF4AA2B8/trigger/1666B887-3CC9-24F8-630C-26B80854583C/activate";
+
 
 // 🔹 Enviar pregunta + código al flujo
 export async function runFlow(question, codigo) {
